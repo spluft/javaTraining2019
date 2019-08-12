9123388ea8dac43c72733266a43eceb193dd3702
spluft <spluft@ya.ru>
2019-08-12 11:45:01 +0300

    module01 task02

--- module01/src/main/java/task02/App.java
+++ module01/src/main/java/task02/App.java
@@ -5,19 +5,27 @@ import java.util.Comparator;
 import java.util.List;
 
 public class App {
+
     public static void main(String[] args) {
         List<Person> people = getPeople();
-        getPeople();
 
         Comparator<Person> compareByNameAsc =
                 (p1, p2) -> p1.getName().compareTo(p2.getName());
         Comparator<Person> compareByAgeDesc =
                 (p1, p2) -> p2.getAge() - p1.getAge();
 
+        sortingListOfPersonByNameAsc(people, compareByNameAsc);
+        sortingListOfPersonByAgeDesc(people,compareByAgeDesc);
+
+    }
+
+    private static void sortingListOfPersonByNameAsc(List<Person> people, Comparator<Person> compareByNameAsc) {
         System.out.println("Sorting list of people by name in ascending order:");
         people.stream().sorted(compareByNameAsc)
                 .forEach(System.out::println);
+    }
 
+    private static void sortingListOfPersonByAgeDesc(List<Person> people, Comparator<Person> compareByAgeDesc){
         System.out.println("\nSorting list of people by age in " +
                 "descendant order:");
         people.stream().sorted(compareByAgeDesc)
