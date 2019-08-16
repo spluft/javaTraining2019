a139ac752920fbb873e3afeb15d6357aa1c93262
spluft <spluft@ya.ru>
2019-08-16 15:11:42 +0300

    module3 Concurrency: task02

---
+++ 5-concurency/src/main/java/task02/App.java
@@ -0,0 +1,55 @@
+package task02;
+
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.TimeUnit;
+
+public class App {
+
+    //I know that CopyOnWriteArrayList create new array during work, but i havn't big array on this app
+    //Should I use synchronized block or some locks or some atomics instead CopyOnWriteArrayList?!
+    private static List<Integer> collection = new CopyOnWriteArrayList<>();
+
+    public static void runThreads() throws InterruptedException {
+        Thread thread1 = new Thread(infinityWritingRandomNumberToCollection);
+        Thread thread2 = new Thread(printSumInCollection);
+        Thread thread3 = new Thread(printSquareSumSquaresNumbersInCollection);
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+
+        thread2.join();
+        thread3.join();
+    }
+
+    private static Runnable infinityWritingRandomNumberToCollection = () -> {
+        while (true) {
+            wait(1);
+            collection.add(new Random().nextInt(10));
+        }
+    };
+
+    private static Runnable printSumInCollection = () -> {
+        wait(5);
+        int sum = collection.stream().reduce(0, Integer::sum);
+        System.out.println("Summary is " + sum);
+    };
+
+    private static Runnable printSquareSumSquaresNumbersInCollection = () -> {
+        wait(5);
+        int sum = collection.stream()
+                .mapToInt(num -> num * num)
+                .reduce(0, Integer::sum);
+        System.out.println("Square of squares is " + sum);
+    };
+
+    private static void wait(int seconds) {
+        try {
+            TimeUnit.SECONDS.sleep(seconds);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
