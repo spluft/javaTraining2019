f96093586cdd8c576187dc69d3d525d9c0f8ef57
spluft <spluft@ya.ru>
2019-08-21 17:35:40 +0300

    Module02 task03: fix for parser: reset tagList.

--- module02/src/main/java/task03XMLparser/parser/dom/DomParser.java
+++ module02/src/main/java/task03XMLparser/parser/dom/DomParser.java
@@ -18,6 +18,8 @@ import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class DomParser implements Parser {
@@ -32,6 +34,7 @@ public class DomParser implements Parser {
     }
 
     private List<Speech> startParse(String path) {
+        tagList = new ArrayList<>();
         Element play = null;
         play = getDocument(path);
         List<Speech> speeches = new ArrayList<>();
