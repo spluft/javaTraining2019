d11a83357c0ec0f84a118c4bff7fffdf5ac3ff61
spluft <spluft@ya.ru>
2019-10-01 18:11:09 +0300

    Module #7 Spring Core: task 02 Annotation-based approach

--- 7-spring_core/src/main/java/task02/configs/Config.java
+++ 7-spring_core/src/main/java/task02/configs/Config.java
@@ -4,8 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import task02.models.Breed;
 import task02.models.Horse;
+import task02.models.Rider;
 import task02.services.EmulationService;
 import task02.services.HorseService;
+import task02.services.RaceService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,25 +21,42 @@ public class Config {
     }
 
     @Bean
-    public HorseService horseService(){
+    public HorseService horseService() {
         HorseService horseService = new HorseService();
         horseService.setHorses(getHorses());
         return horseService;
     }
 
-    private List<Horse> getHorses(){
+    @Bean
+    public RaceService raceService() {
+        HorseService horseService = horseService();
+        RaceService raceService = new RaceService(horseService);
+        return raceService;
+    }
+
+    private List<Horse> getHorses() {
         List<Horse> horses = new ArrayList<>();
         List<Breed> breeds = getBreeds();
-        horses.add(new Horse("1",));
-        horses.add(new Horse("2"));
-        horses.add(new Horse("3"));
+        List<Rider> riders = getRiders();
+        horses.add(new Horse("1", breeds.get(0), 50, riders.get(0)));
+        horses.add(new Horse("2", breeds.get(1), 60, riders.get(1)));
+        horses.add(new Horse("3", breeds.get(2), 70, riders.get(2)));
         return horses;
     }
 
-    private List<Breed> getBreeds(){
+    private List<Breed> getBreeds() {
         List<Breed> breeds = new ArrayList<>();
         breeds.add(new Breed("1"));
         breeds.add(new Breed("2"));
         breeds.add(new Breed("3"));
+        return breeds;
+    }
+
+    private List<Rider> getRiders() {
+        List<Rider> riders = new ArrayList<>();
+        riders.add(new Rider("1",1));
+        riders.add(new Rider("2",2));
+        riders.add(new Rider("3",3));
+        return riders;
     }
 }
