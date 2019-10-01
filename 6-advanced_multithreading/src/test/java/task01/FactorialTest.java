9cf94062d288d383708f675f1b989385c12d8733
spluft <spluft@ya.ru>
2019-10-01 18:37:50 +0300

    Module #6 Multitreading advanced: task01 Factorial fix tests

--- 6-advanced_multithreading/src/test/java/task01/FactorialTest.java
+++ 6-advanced_multithreading/src/test/java/task01/FactorialTest.java
@@ -16,10 +16,29 @@ public class FactorialTest {
     }
 
     @Test
-    public void factorialTest() {
+    public void factorialFJPTest() {
         Assertions.assertEquals(BigInteger.valueOf(6), service.getFactorialWithFJP(4, BigInteger.valueOf(3)));
         Assertions.assertEquals(BigInteger.valueOf(1), service.getFactorialWithFJP(4, BigInteger.valueOf(0)));
         Assertions.assertEquals(BigInteger.valueOf(1), service.getFactorialWithFJP(4, BigInteger.valueOf(1)));
         Assertions.assertThrows(IllegalArgumentException.class, () -> service.getFactorialWithFJP(4, BigInteger.valueOf(-1)));
+    }
+
+    @Test
+    public void factorialWithoutFJPTest() {
+        Assertions.assertEquals(BigInteger.valueOf(6), service.getFactorialWitoutFJP(BigInteger.valueOf(3)));
+        Assertions.assertEquals(BigInteger.valueOf(1), service.getFactorialWitoutFJP(BigInteger.valueOf(0)));
+        Assertions.assertEquals(BigInteger.valueOf(1), service.getFactorialWitoutFJP(BigInteger.valueOf(1)));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> service.getFactorialWitoutFJP(BigInteger.valueOf(-1)));
+    }
+
+    @Test
+    public void compareImpl() {
+        long time1 = System.nanoTime();
+        BigInteger fjFactorial = service.getFactorialWithFJP(4,BigInteger.valueOf(1));
+        long time2 = System.nanoTime();
+        BigInteger factorial = service.getFactorialWitoutFJP(new BigInteger("1"));
+        long time3 = System.nanoTime();
+        Assertions.assertEquals(fjFactorial, factorial);
+        System.out.println("FJP: " + (time2 - time1) + " without FJP: " + (time3 - time2));
     }
 }
